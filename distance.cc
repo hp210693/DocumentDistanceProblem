a78ac27d4ba925f111b12b84ccd7ebfac0155c18
hungphan <hungphan@localhost.localdomain>
2020-04-02 00:54:08 +0700

    add comment

--- distance.cc
+++ distance.cc
@@ -82,7 +82,7 @@ int Distance::ComputeInnerProduct(std::unordered_multimap<std::string, int>& map
  * 
  * @param path_document_first  path of document first
  * @param path_document_second  path of document second
- * @return double 
+ * @return double double the angle between two document
  */
 double ccdist(std::string path_document_first, std::string path_document_second) {
     Distance dis = {path_document_first, path_document_second};
